f9f62c556aa92c002605c85bc7c35b884a9b2fbc
yesid <yesid.manjarres@aule.co>
2021-09-15 17:06:59 -0500

    jejejeje

--- src/styled/Button.jsx
+++ src/styled/Button.jsx
@@ -4,12 +4,12 @@ import { darken } from "polished";
 export const Button = styled.button`
   border: 1px solid transparent;
   padding: 5px 10px;
-  border-radius: 3px;
+  border-radius: 4px;
   font-weight: 500;
   font-size: 15px;
   color: inherit;
+  cursor: pointer;
 `;
-
 export const ContainedButton = styled(Button)`
   background: ${props => props.theme.palette[props.color]};
   color: white;
@@ -21,7 +21,6 @@ export const ContainedButton = styled(Button)`
     )};
   }
 `;
-
 export const OutlinedButton = styled(Button)`
   border-color: ${props => props.theme.palette[props.color]};
   background: transparent;
