989b043329f88b595861109138f0de81ea181d68
yesid <yesid.manjarres@aule.co>
2021-09-15 17:18:28 -0500

    akwdkajsdjkas

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { ContainedButton } from "../../styled/Button";
+import { Container } from "../../styled/Container";
 
 export default function Login() {
 
   function renderUI() {
     return (
-      <div style={{ margin: '10px' }}>
+      <Container>
         <ContainedButton color="primary">Hello world</ContainedButton>
-      </div>
+      </Container>
     );
   }
 
