4b557c5546c61884b92f3db3f830d83997b71d21
yesid <yesid.manjarres@aule.co>
2021-10-08 17:04:57 -0500

    new balance modal added in the summary

--- src/components/main/dashboard/summary/Summary.jsx
+++ src/components/main/dashboard/summary/Summary.jsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 
+import { ContainedButton } from "../../../../styled/Button";
 import { Container, FlexContainer } from "../../../../styled/Container";
 import { Space } from "../../../../styled/Space";
 import { Typography } from "../../../../styled/Typography";
+import NewBalanceModal from "./newBalanceModal/NewBalanceModal";
 
 const SummaryItem = styled.div`
   display: flex;
@@ -12,45 +14,73 @@ const SummaryItem = styled.div`
   flex-direction: column;
   padding: 20px;
   border-radius: ${props => props.theme.borderRadius};
+  background: ${props => props.theme.palette.light};
 `;
 
 export default function Summary() {
+  const [openNewBalanceModal, setOpenNewBalanceModal] = useState(false);
+
+  function handleOpenNewBalanceModal() {
+    setOpenNewBalanceModal(true);
+  }
 
   function renderUI() {
     return (
-      <Container>
-        <Space mt="40" />
-        <Typography size="2" weight="700">Resumen</Typography>
-        <Space mt="40" />
-        <FlexContainer
-          flex
-          justifyContent="start"
-          alignItems="center"
-          color="light"
-        >
+      <>
+        <Container>
+          <Space mt="40" />
+          <FlexContainer
+            flex
+            justifyContent="between"
+            alignItems="center"
+          >
+            <Typography size="1.5" weight="700">Resumen</Typography>
+            <ContainedButton color="primary" onClick={handleOpenNewBalanceModal}>
+              Agregar nuevo balance
+            </ContainedButton>
+          </FlexContainer>
+          <Space mt="40" />
+          <FlexContainer
+            flex
+            justifyContent="evenly"
+            alignItems="center"
+          >
+            <SummaryItem>
+              <Typography size="1.5" weight="600">
+                $370000
+              </Typography>
+              <Space mt="2" />
+              <Typography weight="400">
+                Balance actual
+              </Typography>
+            </SummaryItem>
 
-          <SummaryItem>
-            <Typography size="2.5">
-              $370000
-            </Typography>
-            <Typography weight="700">Balance actual</Typography>
-          </SummaryItem>
+            <SummaryItem>
+              <Typography size="1.5" weight="600">
+                62
+              </Typography>
+              <Space mt="2" />
+              <Typography weight="400">
+                Total de gastos
+              </Typography>
+            </SummaryItem>
 
-          <SummaryItem>
-            <Typography size="2.5">
-              62
-            </Typography>
-            <Typography weight="700">Total de gastos</Typography>
-          </SummaryItem>
-
-          <SummaryItem>
-            <Typography size="2.5">
-              $865700
-            </Typography>
-            <Typography weight="700">Cantidad de gastos</Typography>
-          </SummaryItem>
-        </FlexContainer >
-      </Container>
+            <SummaryItem>
+              <Typography size="1.5" weight="600">
+                $865700
+              </Typography>
+              <Space mt="2" />
+              <Typography weight="400">
+                Cantidad de gastos
+              </Typography>
+            </SummaryItem>
+          </FlexContainer >
+        </Container>
+        <NewBalanceModal
+          showModal={openNewBalanceModal}
+          setShowModal={setOpenNewBalanceModal}
+        />
+      </>
     );
   }
 
---
+++ src/components/main/dashboard/summary/newBalanceModal/NewBalanceModal.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import Modal from "../../../../../styled/Modal";
+import { ContainedButton, OutlinedButton } from "../../../../../styled/Button";
+import { FlexContainer } from "../../../../../styled/Container";
+import { Space } from "../../../../../styled/Space";
+import { TextField } from "../../../../../styled/TextField";
+import { Typography } from "../../../../../styled/Typography";
+
+export default function NewBalanceModal(props) {
+  const { setShowModal } = props;
+
+  function renderUI() {
+    return (
+      <Modal {...props} size="sm">
+        <Typography size="1.3" weight="600">
+          Agregar nuevo balance
+        </Typography>
+        <Space mt="10" />
+        <Typography color="gray">
+          Agrega un nuevo balance a tu cuenta.
+        </Typography>
+        <Space mt="10" />
+        <TextField
+          type="number"
+          placeholder="Nuevo balance"
+          fullWidth
+        />
+        <Space mt="15" />
+        <FlexContainer fluid justifyContent="end">
+          <OutlinedButton
+            color="primary"
+            onClick={() => setShowModal(false)}
+          >
+            Cancelar
+          </OutlinedButton>
+          <Space ml="5" />
+          <ContainedButton color="primary">
+            Confirmar
+          </ContainedButton>
+        </FlexContainer>
+      </Modal>
+    );
+  }
+
+  return renderUI();
+}
