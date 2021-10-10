c92c6766802c9f0c475d425402af2452d2888b1a
Siddev <ewright.develop@gmail.com>
2021-10-09 23:02:09 -0500

    more examples

--- src/components/main/dashboard/summary/expendTable/ExpendTable.jsx
+++ src/components/main/dashboard/summary/expendTable/ExpendTable.jsx
@@ -35,6 +35,9 @@ export default function ExpendTable() {
           {renderTableRow("Hamburguesa", "12000", "10/12/2021")}
           {renderTableRow("Ropa", "10000", "10/12/2021")}
           {renderTableRow("Mantenimiento", "200000", "10/12/2021")}
+          {renderTableRow("Forro de celular", "15000", "10/12/2021")}
+          {renderTableRow("Memoria Ssd", "157000", "10/12/2021")}
+          {renderTableRow("Almuerzo", "11300", "10/12/2021")}
         </tbody>
       </Table>
     );
