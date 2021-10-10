d34006493eb3a2bce312d49b4fa275fdfd7afc6f
Siddev <ewright.develop@gmail.com>
2021-10-10 10:02:52 -0500

    default button modified

--- src/styled/Button.jsx
+++ src/styled/Button.jsx
@@ -11,8 +11,9 @@ export const Button = styled.button`
       props.theme.components.button.size.sm};
   border-radius: ${props => props.theme.borderRadius};
   font-weight: 500;
+  background: ${props => lighten(0.55, props.theme.palette.primary)};
   width: ${props => props.fullWidth && "100%"};
-  color: ${props => props.theme.palette.dark};
+  color: ${props => props.theme.palette.primary};
   font-size: ${props => props.theme.components.button.fontSize};
   :disabled {
     background: #ccc;
