5fcc03afec62d002246603302bd27955491850e4
Siddev <ewright.develop@gmail.com>
2021-10-09 19:19:13 -0500

    space adjustment

--- src/styled/Space.jsx
+++ src/styled/Space.jsx
@@ -1,13 +1,13 @@
 import styled from "styled-components";
 
 export const Space = styled.div`
-  margin-top: ${props => props.mt}px;
-  margin-bottom: ${props => props.mb}px;
-  margin-left: ${props => props.ml}px;
-  margin-right: ${props => props.mr}px;
+  margin-top: ${props => props.mt || 0}px;
+  margin-bottom: ${props => props.mb || 0}px;
+  margin-left: ${props => props.ml || 0}px;
+  margin-right: ${props => props.mr || 0}px;
 
-  padding-top: ${props => props.pt}px;
-  padding-bottom: ${props => props.pb}px;
-  padding-left: ${props => props.pl}px;
-  padding-right: ${props => props.pr}px;
+  padding-top: ${props => props.pt || 0}px;
+  padding-bottom: ${props => props.pb || 0}px;
+  padding-left: ${props => props.pl || 0}px;
+  padding-right: ${props => props.pr || 0}px;
 `;
